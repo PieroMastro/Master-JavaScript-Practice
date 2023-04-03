243bfd4a591b01a2af6544dc093de69cc1b9c7c7
PieroMastro <pier.andres@gmail.com>
2023-04-03 02:59:15 +0000

    refactoring some excercises

--- exercises/066-isEitherEvenOrAreBoth7/app.js
+++ exercises/066-isEitherEvenOrAreBoth7/app.js
@@ -1,4 +1,10 @@
+
 function isEitherEvenOrAreBoth7(num1, num2) {
-    // your code here
-    
+    return num1 % 2 === 0 || num2 % 2 === 0 ? true :
+        num1 === 7 && num2 === 7 ? true :
+            false;
+
 }
+
+let output = isEitherEvenOrAreBoth7(3, 7);
+console.log(output); // --> false

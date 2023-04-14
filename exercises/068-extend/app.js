7007f58092fdf56fbfa923a5dbd1d93aa7f02971
PieroMastro <pier.andres@gmail.com>
2023-04-14 03:49:05 +0000

    70 done

--- exercises/068-extend/app.js
+++ exercises/068-extend/app.js
@@ -1,13 +1,18 @@
 let obj1 = {
-    a: 1,
-    b: 2
+  a: 1,
+  b: 2
 };
 let obj2 = {
-    b: 4,
-    c: 3
+  b: 4,
+  c: 3
 };
 
 function extend(obj1, obj2) {
-    // your code here
-
+  for (let prop in obj2) {
+    obj1[prop] = obj2[prop];
+  }
+  return obj1;
 }
+
+console.log(obj1); // --> {a: 1, b: 2, c: 3}
+console.log(obj2); // --> {b: 4, c: 3}
